ed6e817ad331507458f282cca1c1df33d315d7e1
Grzegorz Olejarczyk <127106396+Beard2Man@users.noreply.github.com>
2023-09-01 13:55:01 +0200

    Creating navigation and taking you to the product page when you click on a link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Products from "./templates/Products";
+import Navigation from "./mainPages/Navigation";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navigation />
         <Routes>
           <Route path="/" element={<Products />} />
           {/* <Route path="/products/:productId" element={<ProductsDetail />} /> */}
---
+++ src/mainPages/Navigation.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import React, { useState, useContext, useEffect } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+import Logo from "../assets/immutable/img/Logo.png";
+
+import "../scss/layout/_navigation.scss";
+
+function Navigation() {
+  const [tabs, setTabs] = useState([]);
+  const [error, setError] = useState(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/tabs")
+      .then((response) => {
+        setTabs(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+        console.error("API data download error", err);
+      });
+  }, []);
+  return (
+    <nav className="navbar">
+      <a href="/" className="logo">
+        <img src={Logo} alt="Logo" />
+      </a>
+      <div className={`nav-links ${isMenuOpen ? "mobile-menu" : ""}`}>
+        <ul>
+          {tabs.map((tab) => (
+            <li key={tab.id}>
+              <a href={tab.url}>{tab.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <FontAwesomeIcon
+        icon={isMenuOpen ? faTimes : faBars}
+        className="menu-hamburger"
+        onClick={handleMenuToggle}
+      />
+    </nav>
+  );
+}
+
+export default Navigation;
